9baecbb87fd41d05301dabacddd823a4ff7a2fb6
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-09 13:36:20 +0100

    finish static server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use("/beetlejuice", express.static("beetlejuice"));
+
 app.listen(5000,() =>{
     console.log("server is listening on port 5000");
 });
