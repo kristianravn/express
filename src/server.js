74140428fcc9c27ff9bc835653b5f5e168ad204e
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-09 13:49:18 +0100

    remove static server code

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/beetlejuice", express.static("beetlejuice"));
-
 app.listen(5000,() =>{
     console.log("server is listening on port 5000");
 });
