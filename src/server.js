a50a1b78b7d14ce64ad16ad43ccbac55e527f415
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-10 15:31:04 +0100

    add post route for books

--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
+
+
+const connection = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB is working");
+};
+
+connection();
+
+
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+
+    },
+});
+
+const Book = mongoose.model("book", bookSchema);
+
+
 app.get("/books/allbooks",(request, response) =>{
-    
     response.send({message: "success"});
  });
 
@@ -15,9 +47,15 @@ app.get("/books", (request, response) => {
 
  });
 
-app.post("/books",(request,response) =>{
+app.post("/books/addbook", async (request,response) =>{
+
+    const book = await Book.create({
+        title: request.body.title,
+        author: request.body.author,
+        genre: request.body.genre,
+    });
    
-    response.send({message: "success"});
+    response.send({message: "success", book: book});
 });
 
 app.listen(5000,() =>{
