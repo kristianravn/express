916ed51bedb4c83df1c129721ec645b48050a0e7
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-10 10:47:26 +0100

    add routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,32 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+
+const fakeDB = [];
+
+app.get("/books/allbooks",(request, response) =>{
+    
+    response.send({message: "success", books: fakeDB});
+ });
+
+app.get("/books", (request, response) => {
+    const book = {
+        title: "esv bible",
+        author: "God",
+        genre: "history",
+    };
+    response.send({message: "success", book: book});
+
+ });
+
+app.post("/books",(request,response) =>{
+   
+    fakeDB.push(request.body);
+    response.send({message: '${request.body.title}has been added'});
+});
+
 app.listen(5000,() =>{
     console.log("server is listening on port 5000");
 });
