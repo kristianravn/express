db909381642bbf70fcf95cd638f600a347c7165c
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-11 16:00:17 +0100

    add update by author

---
+++ objects.js
@@ -0,0 +1,41 @@
+// step 1
+
+// object:myObj
+const myObj = {
+    name: "m",
+    age: 25,
+    isAwsome: true,
+    // key: details
+    details: {
+        computer: "mac",
+       //key: phone
+        phone: "apple",
+        t_shirt: "red"
+    }
+};
+
+const someObj = {
+    name: "ironman",
+    isRich: true
+
+}
+
+// step 2
+
+
+const avengerName = "black widow";
+
+const avenger = {
+    key: "value",
+    name: "ironman",
+    isRich: "true",
+    details: {
+        canFly: false,
+        gender: "female",
+        weapons: "guns",
+    },
+
+}
+
+
+
--- src/server.js
+++ src/server.js
@@ -47,22 +47,53 @@ app.get("/books", (request, response) => {
 
  });
 
-// app.post("/books/addbook", async (request,response) =>{
+// add book with title,author,genre
 
-//     const book = await Book.create({
-//         title: request.body.title,
-//         author: request.body.author,
-//         genre: request.body.genre,
-//     });
+app.post("/books/addbook", async (request,response) =>{
+
+    const book = await Book.create({
+        title: request.body.title,
+        author: request.body.author,
+        genre: request.body.genre,
+    });
    
-//     response.send({message: "success", book: book});
-// });
+    response.send({message: "success", book: book});
+});
 
 app.get("/books/getallbooks", async (request,response) => {
     const books = await Book.find({});
     
     response.send({message: "success", allbooks: books});
 });
+
+// delete one book by title
+
+app.delete("/books/deletebookbytitle", async (request,response) => {
+   const books = await Book.deleteOne({
+        title: request.body.title,
+
+   
+   });
+
+   response.send ({message: "success", deletebookbytitle: books})
+
+});
+
+
+// updating a document on db (a book) filter book by title
+
+app.put ("/books/updatebookbyauthor", async(request,response) =>{
+    const books = await Book.updatebook({
+        title: request.body.title,
+
+   
+   });
+   response.send ({message: "success", updatebook: books})
+})
+
+
+
+
 
 app.listen(5000,() =>{
     console.log("server is listening on port 5000");
